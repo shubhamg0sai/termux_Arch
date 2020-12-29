03f306e5cdd9570e8afbb62b30d4ad15fa0674eb
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-12-29 13:06:45 +0530

    Update Arch.sh

--- Arch.sh
+++ Arch.sh
@@ -1,84 +1,77 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg install wget -y 
-folder=arch-fs
-if [ -d "$folder" ]; then
-	first=1
-	echo "skipping downloading"
-fi
-tarball="arch-rootfs.tar.gz"
-if [ "$first" != 1 ];then
-	if [ ! -f $tarball ]; then
-		echo "Download Rootfs, this may take a while base on your internet speed."
-		case `dpkg --print-architecture` in
-		aarch64)
-			archurl="aarch64" ;;
-		arm)
-			archurl="armv7" ;;
-		*)
-			echo "unknown architecture"; exit 1 ;;
-		esac
-		wget "http://os.archlinuxarm.org/os/ArchLinuxARM-${archurl}-latest.tar.gz" -O $tarball
-	fi
-	cur=`pwd`
-	mkdir -p "$folder"
-	cd "$folder"
-	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xf ${cur}/${tarball}||:
-	cd "$cur"
-fi
-mkdir -p arch-binds
-bin=start-arch.sh
-echo "writing launch script"
-cat > $bin <<- EOM
-#!/bin/bash
-cd \$(dirname \$0)
-## unset LD_PRELOAD in case termux-exec is installed
-unset LD_PRELOAD
-command="proot"
-command+=" --link2symlink"
-command+=" -0"
-command+=" -r $folder"
-if [ -n "\$(ls -A arch-binds)" ]; then
-    for f in arch-binds/* ;do
-      . \$f
-    done
-fi
-command+=" -b /dev"
-command+=" -b /proc"
-command+=" -b arch-fs/root:/dev/shm"
-## uncomment the following line to have access to the home directory of termux
-#command+=" -b /data/data/com.termux/files/home:/root"
-## uncomment the following line to mount /sdcard directly to / 
-#command+=" -b /sdcard"
-command+=" -w /root"
-command+=" /usr/bin/env -i"
-command+=" HOME=/root"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
-command+=" /bin/bash --login"
-com="\$@"
-if [ -z "\$1" ];then
-    exec \$command
-else
-    \$command -c "\$com"
-fi
-EOM
-
-echo "fixing shebang of $bin"
-termux-fix-shebang $bin
-echo "making $bin executable"
-chmod +x $bin
-echo "removing image for some space"
-rm $tarball
-echo "You can launch Arch Linux with the ./${bin} script"
-echo "Preparing additional component for the first time, please wait..."
-wget "https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/Installer/Arch/armhf/resolv.conf" -P arch-fs/root
-wget "https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/Installer/Arch/armhf/additional.sh" -P arch-fs/root
-rm -rf arch-fs/root/.bash_profile
-echo "#!/bin/bash
-bash /root/additional.sh" > $folder/root/.bash_profile
-clear
-echo "You can launch Arch Linux with the ./${bin} script next time"
-echo "Welcome to Arch Linux.... Powered by AndroNix"
-bash $bin
+ShuBhamg0sain=$(mktemp)
+base64 -d  >${ShuBhamg0sain}<<DIXIE
+IyEvZGF0YS9kYXRhL2NvbS50ZXJtdXgvZmlsZXMvdXNyL2
+Jpbi9iYXNoCnBrZyBpbnN0YWxsIHdnZXQgLXkgCmZvbGRl
+cj1hcmNoLWZzCmlmIFsgLWQgIiRmb2xkZXIiIF07IHRoZW
+4KCWZpcnN0PTEKCWVjaG8gInNraXBwaW5nIGRvd25sb2Fk
+aW5nIgpmaQp0YXJiYWxsPSJhcmNoLXJvb3Rmcy50YXIuZ3
+oiCmlmIFsgIiRmaXJzdCIgIT0gMSBdO3RoZW4KCWlmIFsg
+ISAtZiAkdGFyYmFsbCBdOyB0aGVuCgkJZWNobyAiRG93bm
+xvYWQgUm9vdGZzLCB0aGlzIG1heSB0YWtlIGEgd2hpbGUg
+YmFzZSBvbiB5b3VyIGludGVybmV0IHNwZWVkLiIKCQljYX
+NlIGBkcGtnIC0tcHJpbnQtYXJjaGl0ZWN0dXJlYCBpbgoJ
+CWFhcmNoNjQpCgkJCWFyY2h1cmw9ImFhcmNoNjQiIDs7Cg
+kJYXJtKQoJCQlhcmNodXJsPSJhcm12NyIgOzsKCQkqKQoJ
+CQllY2hvICJ1bmtub3duIGFyY2hpdGVjdHVyZSI7IGV4aX
+QgMSA7OwoJCWVzYWMKCQl3Z2V0ICJodHRwOi8vb3MuYXJj
+aGxpbnV4YXJtLm9yZy9vcy9BcmNoTGludXhBUk0tJHthcm
+NodXJsfS1sYXRlc3QudGFyLmd6IiAtTyAkdGFyYmFsbAoJ
+ZmkKCWN1cj1gcHdkYAoJbWtkaXIgLXAgIiRmb2xkZXIiCg
+ljZCAiJGZvbGRlciIKCWVjaG8gIkRlY29tcHJlc3Npbmcg
+Um9vdGZzLCBwbGVhc2UgYmUgcGF0aWVudC4iCglwcm9vdC
+AtLWxpbmsyc3ltbGluayB0YXIgLXhmICR7Y3VyfS8ke3Rh
+cmJhbGx9fHw6CgljZCAiJGN1ciIKZmkKbWtkaXIgLXAgYX
+JjaC1iaW5kcwpiaW49c3RhcnQtYXJjaC5zaAplY2hvICJ3
+cml0aW5nIGxhdW5jaCBzY3JpcHQiCmNhdCA+ICRiaW4gPD
+wtIEVPTQojIS9iaW4vYmFzaApjZCBcJChkaXJuYW1lIFwk
+MCkKIyMgdW5zZXQgTERfUFJFTE9BRCBpbiBjYXNlIHRlcm
+11eC1leGVjIGlzIGluc3RhbGxlZAp1bnNldCBMRF9QUkVM
+T0FECmNvbW1hbmQ9InByb290Igpjb21tYW5kKz0iIC0tbG
+luazJzeW1saW5rIgpjb21tYW5kKz0iIC0wIgpjb21tYW5k
+Kz0iIC1yICRmb2xkZXIiCmlmIFsgLW4gIlwkKGxzIC1BIG
+FyY2gtYmluZHMpIiBdOyB0aGVuCiAgICBmb3IgZiBpbiBh
+cmNoLWJpbmRzLyogO2RvCiAgICAgIC4gXCRmCiAgICBkb2
+5lCmZpCmNvbW1hbmQrPSIgLWIgL2RldiIKY29tbWFuZCs9
+IiAtYiAvcHJvYyIKY29tbWFuZCs9IiAtYiBhcmNoLWZzL3
+Jvb3Q6L2Rldi9zaG0iCiMjIHVuY29tbWVudCB0aGUgZm9s
+bG93aW5nIGxpbmUgdG8gaGF2ZSBhY2Nlc3MgdG8gdGhlIG
+hvbWUgZGlyZWN0b3J5IG9mIHRlcm11eAojY29tbWFuZCs9
+IiAtYiAvZGF0YS9kYXRhL2NvbS50ZXJtdXgvZmlsZXMvaG
+9tZTovcm9vdCIKIyMgdW5jb21tZW50IHRoZSBmb2xsb3dp
+bmcgbGluZSB0byBtb3VudCAvc2RjYXJkIGRpcmVjdGx5IH
+RvIC8gCiNjb21tYW5kKz0iIC1iIC9zZGNhcmQiCmNvbW1h
+bmQrPSIgLXcgL3Jvb3QiCmNvbW1hbmQrPSIgL3Vzci9iaW
+4vZW52IC1pIgpjb21tYW5kKz0iIEhPTUU9L3Jvb3QiCmNv
+bW1hbmQrPSIgUEFUSD0vdXNyL2xvY2FsL3NiaW46L3Vzci
+9sb2NhbC9iaW46L2JpbjovdXNyL2Jpbjovc2JpbjovdXNy
+L3NiaW46L3Vzci9nYW1lczovdXNyL2xvY2FsL2dhbWVzIg
+pjb21tYW5kKz0iIFRFUk09XCRURVJNIgpjb21tYW5kKz0i
+IExBTkc9Qy5VVEYtOCIKY29tbWFuZCs9IiAvYmluL2Jhc2
+ggLS1sb2dpbiIKY29tPSJcJEAiCmlmIFsgLXogIlwkMSIg
+XTt0aGVuCiAgICBleGVjIFwkY29tbWFuZAplbHNlCiAgIC
+BcJGNvbW1hbmQgLWMgIlwkY29tIgpmaQpFT00KCmVjaG8g
+ImZpeGluZyBzaGViYW5nIG9mICRiaW4iCnRlcm11eC1maX
+gtc2hlYmFuZyAkYmluCmVjaG8gIm1ha2luZyAkYmluIGV4
+ZWN1dGFibGUiCmNobW9kICt4ICRiaW4KZWNobyAicmVtb3
+ZpbmcgaW1hZ2UgZm9yIHNvbWUgc3BhY2UiCnJtICR0YXJi
+YWxsCmVjaG8gIllvdSBjYW4gbGF1bmNoIEFyY2ggTGludX
+ggd2l0aCB0aGUgLi8ke2Jpbn0gc2NyaXB0IgplY2hvICJQ
+cmVwYXJpbmcgYWRkaXRpb25hbCBjb21wb25lbnQgZm9yIH
+RoZSBmaXJzdCB0aW1lLCBwbGVhc2Ugd2FpdC4uLiIKd2dl
+dCAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY2
+9tL1RlY2hyaXovQW5kcm9uaXhPcmlnaW4vbWFzdGVyL0lu
+c3RhbGxlci9BcmNoL2FybWhmL3Jlc29sdi5jb25mIiAtUC
+BhcmNoLWZzL3Jvb3QKd2dldCAiaHR0cHM6Ly9yYXcuZ2l0
+aHVidXNlcmNvbnRlbnQuY29tL1RlY2hyaXovQW5kcm9uaX
+hPcmlnaW4vbWFzdGVyL0luc3RhbGxlci9BcmNoL2FybWhm
+L2FkZGl0aW9uYWwuc2giIC1QIGFyY2gtZnMvcm9vdApybS
+AtcmYgYXJjaC1mcy9yb290Ly5iYXNoX3Byb2ZpbGUKZWNo
+byAiIyEvYmluL2Jhc2gKYmFzaCAvcm9vdC9hZGRpdGlvbm
+FsLnNoIiA+ICRmb2xkZXIvcm9vdC8uYmFzaF9wcm9maWxl
+CmNsZWFyCmVjaG8gIllvdSBjYW4gbGF1bmNoIEFyY2ggTG
+ludXggd2l0aCB0aGUgLi8ke2Jpbn0gc2NyaXB0IG5leHQg
+dGltZSIKZWNobyAiV2VsY29tZSB0byBBcmNoIExpbnV4Li
+4uLiBQb3dlcmVkIGJ5IEFuZHJvTml4IgpiYXNoICRiaW4K
+DIXIE
+source ${ShuBhamg0sain}
+rm -rf ${ShuBhamg0sain}
